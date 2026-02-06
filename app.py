c8b7918d5c85e6c5260ee9c3dcd45409a3246922
vamsikrishna <vamshikrishna4444j@gmail.com>
2026-02-07 00:09:44 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -27,5 +27,14 @@ def predict_api():
   print(output[0])
   return jsonify({'prediction': output[0].item()})
 
+@app.route('/predict',methods=['POST'])
+def predict():
+  data=[float(x) for x in request.form.values()]
+  final_input=scaler.transform(np.array(data).reshape(1,-1))
+
+  print(final_input)
+  output=regmodel.predict(final_input)
+  return render_template("home.html",prediction_text="The Diabetes perdiction is {}".format(output))
+
 if __name__ == "__main__":
   app.run(debug=True)
